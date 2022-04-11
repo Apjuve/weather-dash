db26beb2915db5beb4beb69bb74aeafc703ca740
Apjuve <abiponce3@hotmail.com>
2022-04-10 20:07:48 -0400

    finished local stooraage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,11 +123,18 @@ function makeCityList (input) {
     createdContainer.append(createdPEl);
     createdRow.append(createdContainer);
     cityNamesList.append(createdRow);
-} 
+
+    createdPEl.addEventListener("click", function() {
+        var definedValue = createdPEl.innerHTML;
+        cityPrevSearch(definedValue);
+});
+
+}
 
 clearBtn.addEventListener("click", function () {
     cityNamesList.innerHTML = '';  
 })
+
 
 
 function cityPrevSearch (val) {
@@ -191,6 +198,7 @@ fetch(cityWeatherURL)
             currentUV.textContent = uv;
 
             fiveDayForecast(currenti);
+            saveSearch();
         })
     })
 }
@@ -265,7 +273,19 @@ function saveSearch () {
         }
 
         var dateArray = JSON.parse(localStorage.getItem("dateArray"));
-        console.log(dateArray);
+        var iconArray = JSON.parse(localStorage.getItem("iconArray"));
+        var tempArray = JSON.parse(localStorage.getItem("tempArray"));   
+        var windArray = JSON.parse(localStorage.getItem("windArray"));
+        var humidityArray = JSON.parse(localStorage.getItem("humidityArray"));  
+
+        for(var i = 0; i < weeklyWeatherBlocks.length; i++) {
+            weeklyDate[i].textContent = dateArray[i];
+            weeklyIcon[i].setAttribute("src",iconArray[i]);
+            weeklyTemp[i].textContent = tempArray[i];
+            weeklyWind[i].textContent = windArray[i];
+            weeklyHumidity[i].textContent = humidityArray[i];
+        }
+
     }
 
     showLastSearch(); 
